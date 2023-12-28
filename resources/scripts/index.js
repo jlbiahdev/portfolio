3370f28b9e18bddcf69c56598b456c74cc378f7a
jlbiahdev <jeanlucbiah.dev@outlook.com>
2023-12-28 23:40:21 +0100

    minor changes

--- resources/scripts/index.js
+++ resources/scripts/index.js
@@ -1,3 +1,5 @@
+document.getElementById('mailform').classList.add('hide');
+
 var radio_fr = document.getElementById("flag-fr");
 var radio_en = document.getElementById("flag-en");
 
@@ -32,4 +34,35 @@ radio_fr.onchange = function() {
         elt.classList.remove('show');
     });
   }
+}
+
+function openMailForm() {
+  document.getElementById('mailform').classList.add('show'); 
+}
+
+function sendEmail() {
+  // 
+  Email.send({
+      SecureToken : "962c8d01-d47d-4dd7-b0d1-9c987cd3bf01",
+      To : "raheem.dev.cyrano@gmail.com",
+      From : "contact.cyranoc@gmail.com",
+      Subject : 'Contact from your portfolio',
+      Body : "Name : " + document.getElementById("name").value
+              + "<br /> Sender Email : " + document.getElementById("email").value
+              + "<br /> Phone : " + document.getElementById("phone").value
+              + "<br /> Message : <br />>" + document.getElementById("msg-body").value
+  }).then(message => alert(message).catch(err => alert(err))
+  );
+  
+  document.getElementById('name').value = '';
+  document.getElementById('email').value = '';
+  document.getElementById('phone').value = '';
+  document.getElementById('msg-body').value = '';
+
+  let item = document.getElementById('mailform');
+  console.log('sendEmail');
+  item.classList.add('hide'); 
+  item.classList.remove('show');
+
+  return true;
 }
