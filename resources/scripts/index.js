0649e25807715a42ad0e3682cab68e490717546d
jlbiahdev <jeanlucbiah.dev@outlook.com>
2023-12-31 00:46:34 +0100

    Adds mail sending

--- resources/scripts/index.js
+++ resources/scripts/index.js
@@ -1,12 +1,12 @@
-document.getElementById('mailform').classList.add('hide');
+// document.getElementById('mailform').classList.add('hide');
 
-var radio_fr = document.getElementById("flag-fr");
-var radio_en = document.getElementById("flag-en");
+var radio_fr = document.getElementById('flag-fr');
+var radio_en = document.getElementById('flag-en');
 
 radio_en.onchange = function() {
     console.log('radio_en.onchange')
   if (radio_en.checked === true) {
-    //var elts = document.getElementsByClassName("french")
+    //var elts = document.getElementsByClassName('french')
     
     document.querySelectorAll('.french').forEach(function(elt) {
         elt.classList.add('hide');
@@ -41,28 +41,42 @@ function openMailForm() {
 }
 
 function sendEmail() {
-  // 
-  Email.send({
-      SecureToken : "962c8d01-d47d-4dd7-b0d1-9c987cd3bf01",
-      To : "raheem.dev.cyrano@gmail.com",
-      From : "contact.cyranoc@gmail.com",
-      Subject : 'Contact from your portfolio',
-      Body : "Name : " + document.getElementById("name").value
-              + "<br /> Sender Email : " + document.getElementById("email").value
-              + "<br /> Phone : " + document.getElementById("phone").value
-              + "<br /> Message : <br />>" + document.getElementById("msg-body").value
-  }).then(message => alert(message).catch(err => alert(err))
-  );
-  
-  document.getElementById('name').value = '';
-  document.getElementById('email').value = '';
-  document.getElementById('phone').value = '';
-  document.getElementById('msg-body').value = '';
+  let name = document.getElementById('name').value;
+  let email = document.getElementById('email').value;
+  let phone = document.getElementById('phone').value;
+  let msg = document.getElementById('msg-body').value.replace(/\r?\n/g, '<br />');
 
+  if (name === '' || name === undefined || name === null) {
+    alert(`Name can't be empty`);
+    return;
+  }
+
+  if (email === '' || email === undefined || email === null) {
+    alert(`The email can't be empty`);
+    return;
+  }
+
+  if (msg === '' || msg === undefined || msg === null) {
+    alert(`The message body can't be empty`);
+    return;
+  }
+
+  Email
+  .send({
+  SecureToken : 'f4985288-468e-4499-bd1b-338b7dfa3eb2',
+  To : 'raheem.dev.cyrano@gmail.com',
+      From : 'contact.cyranoc@gmail.com',
+      Subject : '[Portfolio] New contact from your web page',
+      Body : '<strong>Name :</strong> ' + name
+              + '<br /> <strong>Sender Email :</strong> ' + email
+              + '<br /> <strong>Phone :</strong> ' + phone
+              + '<br /> <strong>Message:</strong> <br />' + msg
+      })
+  .then(message => alert(message))
+  .catch(err => alert(err));
+  
   let item = document.getElementById('mailform');
   console.log('sendEmail');
   item.classList.add('hide'); 
   item.classList.remove('show');
-
-  return true;
-}
+  }
